d517eab7d1fcb8f6f9c2e79aca6bd8465a3f5991
Gagan <g.deepsingh1@gmail.com>
2018-05-18 02:20:40 +0530

    file reading

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var app1 = require('./app1');
 var _ = require('lodash');
+var data = require('./data.json');
+var fs = require('fs');
 
+console.log(data);
 console.log(app1.text);
 console.log(_.random(1,90));
+
+fs.readFile('./data.json', 'utf-8', function(err, data) {
+  console.log(JSON.parse(data));
+});
