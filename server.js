b0b5fba902532890c0acd9a22a9e918dc5583ee9
Gagan <g.deepsingh1@gmail.com>
2018-05-23 19:39:15 +0530

    adding socket

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -15,9 +17,14 @@ app.get('/messages', (req, res) => {
 });
 app.post('/messages', (req, res) => {
   messages.push(req.body);
+  io.emit('message', req.body);
   res.sendStatus(200);
 });
 
-var server = app.listen(9000, function() {
+io.on('connection', (socket) => {
+  console.log('user connected');
+});
+
+var server = http.listen(9000, function() {
   console.log('server is listening on port', server.address().port);
 });
