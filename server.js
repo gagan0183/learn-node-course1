f23070f2cdc370bdbe43453ef5a13066297a1909
Gagan <g.deepsingh1@gmail.com>
2018-05-24 00:05:41 +0530

    mongoose integrate

--- server.js
+++ server.js
@@ -3,26 +3,44 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var messages = [
-  {name: 'P', text: 'This is p'},
-  {name: 'PP', text: 'This is pp'}
-];
+var dbUrl = "mongodb://ppp:ARIHANT@ds231720.mlab.com:31720/nodecourse";
+
+var Message = mongoose.model('Message', {
+  name: String,
+  text: String
+});
+
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+      if(err) {
+        res.sendStatus(500);
+      }
+      res.send(messages);
+    });
 });
 app.post('/messages', (req, res) => {
-  messages.push(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
+  var message = new Message(req.body);
+  message.save((err) => {
+    if(err) {
+      res.sendStatus(500);
+    }
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  });
 });
 
 io.on('connection', (socket) => {
   console.log('user connected');
+});
+
+mongoose.connect(dbUrl, function(err) {
+  console.log('mongodb connection', err);
 });
 
 var server = http.listen(9000, function() {
